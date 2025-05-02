ada7d7ae4435427fddc5ff493ad478e8de6d1d48
tauda27 <tauda27@gmail.com>
2025-05-01 18:27:18 -0600

    my website

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+    document: refers to the current document
+    getElementById: it gets the element based on id value
+    src: tells you the source
+    function: saves your code for later (you can tell the image when to appear)
+    onclick: adds an event to any object
+
+    */
+
